1db0b2c5c02609ba8ecd4f90ba5330ede629e31f
Anish Thite <Anish Thite>
2016-12-20 02:00:56 -0500

    dnej

--- VowelsRUs/src/VowelsRUsAnishThite.java
+++ VowelsRUs/src/VowelsRUsAnishThite.java
@@ -15,9 +15,9 @@ public class VowelsRUsAnishThite {
 	private static InputStreamReader inReader;
 	private static BufferedReader reader;
 	private StringTokenizer strTkn;
-	private static String line, ending, plural;
+	private static String line, ending, plural, suffixStart;
 	
-	private static boolean vowel;
+	private static boolean vowel, doubleVowel, consonant, doubleConsonant;
 	
 	public static void main(String[] args) {
 	//Steps:
@@ -44,24 +44,109 @@ public class VowelsRUsAnishThite {
 		ending = ending.toLowerCase();
 	}
 	// checks ending to see if it is a vowel
-	public static boolean check_ending(){
-		int pos = 0;
-		while ( pos <= ending.length()){
-				if (ending.substring(pos) == "a" || 
-					ending.substring(pos) == "c" || 
-					ending.substring(pos) == "s" || 
-					ending.substring(pos) == "l"  )
+	public static boolean check_vowel(){
+				if (ending.substring(ending.length()-1) == "a" || 
+					ending.substring(ending.length()-1) == "c" || 
+					ending.substring(ending.length()-1) == "s" || 
+					ending.substring(ending.length()-1) == "l"  )
 				{
 				vowel = true;
 				}
-		pos++;
+				else{
+				vowel = false;
+				}
+			return vowel;
+	}
+	//checks to see for a double value
+	public static boolean check_doubleVowel(){
+		if ((ending.substring(ending.length()-2) == "a" || 
+			ending.substring(ending.length()-2) == "c" || 
+			ending.substring(ending.length()-2) == "s" || 
+			ending.substring(ending.length()-2) == "l" ) && (vowel == true))
+		{
+		doubleVowel = true;
 		}
-		return vowel;
+		else{
+		doubleVowel = false;
+		}
+	return doubleVowel;
+	}
+	public static boolean check_consonant(){
+		if ((ending.substring(ending.length()-1) != "a" || 
+			ending.substring(ending.length()-1) != "c" || 
+			ending.substring(ending.length()-1) != "s" || 
+			ending.substring(ending.length()-1) != "l" ) && (vowel == false))
+		{
+		consonant = true;
+		}
+		else{
+		consonant = false;
+		}
+	return consonant;
+	}	
+	public static boolean check_doubleConsonant(){
+		if ((ending.substring(ending.length()-2) != "a"|| 
+			ending.substring(ending.length()-2) != "c" || 
+			ending.substring(ending.length()-2) != "s" || 
+			ending.substring(ending.length()-2) != "l" ) && (consonant == true))
+		{
+		doubleConsonant = true;
+		}
+		else{
+		doubleConsonant = false;
+		}
+	return doubleConsonant;
+	}
+	public static String check_suffixStart(){
+		if (ending.substring(0) == "a"|| 
+			ending.substring(0) == "c" || 
+			ending.substring(0) == "s" || 
+			ending.substring(0) == "l")
+		{
+		suffixStart = "vowel";
+		}
+		else{
+		suffixStart = "consonant";
+		}
+	return suffixStart;
 	}
 	//changes word to plural
 	public static String change_plural(){
-		
-		
+		//change based on vowel
+		if ((vowel == true) && (doubleVowel != true)){
+			
+		}
+		//change based on double vowel
+		if ((vowel == true) && (doubleVowel == true)){
+			
+		}
+		//change based on consonant
+		if ((consonant == true) && (doubleConsonant != true)){
+			
+		}
+		//change based on double consonant
+		if ((consonant == true) && (doubleConsonant == true)){
+			
+		}
+		return plural;
+	}
+	public static String change_suffix(){
+		//change based on vowel
+		if ((vowel == true) && (doubleVowel != true)){
+			
+		}
+		//change based on double vowel
+		if ((vowel == true) && (doubleVowel == true)){
+			
+		}
+		//change based on consonant
+		if ((consonant == true) && (doubleConsonant != true)){
+			
+		}
+		//change based on double consonant
+		if ((consonant == true) && (doubleConsonant == true)){
+			
+		}
 		return plural;
 	}
 }
